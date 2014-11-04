c8ea6192254f40847cc4758edff184c13e820c49
kyo_ago <kyo@0-9.sakura.ne.jp>
2014-11-04 12:18:52 +0900

    Update protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,8 +1,8 @@
 exports.config = {
-	capabilities: {
-		'browserName': 'chrome'
-	},
-	sauceUser: 'SauseLabUserName',
-	sauceKey: 'SauseLabAccessKey',
-	specs: ['spec.js']
+  capabilities: {
+    'browserName': 'chrome'
+  },
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+  specs: ['spec.js']
 };
