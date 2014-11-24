e27fde85a4782bb76eebd9316ecb0d15434ca4f9
kyo_ago <kyo@0-9.sakura.ne.jp>
2014-11-24 18:25:05 +0900

    Update spec.js

--- spec.js
+++ spec.js
@@ -1,6 +1,6 @@
 describe('angularjs homepage todo list', function() {
   it('should add a todo', function() {
-    browser.get('http://example.local/index.html');
+    browser.get('http://example.local:8000/index.html');
     var links = element.all(by.repeater('a'));
     expect(links.count()).toEqual(6);
   });
