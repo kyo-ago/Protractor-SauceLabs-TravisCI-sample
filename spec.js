13002d143574c569843bca8351440d55200dcc07
kyo_ago <kyo@0-9.sakura.ne.jp>
2014-11-25 16:14:28 +0900

    Update spec.js

--- spec.js
+++ spec.js
@@ -1,9 +1,9 @@
 describe('Protractor-SauceLabs-TravisCI-sample', function() {
   browser.ignoreSynchronization = true;
   it('should get a link', function() {
-    browser.get('http://example.local:8000/');
+    browser.get('http://example.local:8000/index.html');
     var links = element.all(by.repeater('a'));
-    console.log(element.all(by.repeater('body')).get(0).getText());
+    console.log(links.count());
     expect(links.count()).toEqual(1);
   });
 });
