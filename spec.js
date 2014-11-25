a5e79d2c9d2514612052b452ba69984e8a831f7b
kyo_ago <kyo@0-9.sakura.ne.jp>
2014-11-25 16:53:18 +0900

    Update spec.js

--- spec.js
+++ spec.js
@@ -2,8 +2,7 @@ describe('Protractor-SauceLabs-TravisCI-sample', function() {
   browser.ignoreSynchronization = true;
   it('should get a link', function() {
     browser.get('http://example.local:8000/index.html');
-    var links = element.all(by.repeater('a'));
-    console.log(links.count());
+    var links = element.all(by.css('a'));
     expect(links.count()).toEqual(1);
   });
 });
