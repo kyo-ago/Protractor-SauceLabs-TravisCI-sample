52614810c62b1fc6d6b550551817b90053514fe6
kyo_ago <kyo@0-9.sakura.ne.jp>
2014-11-27 20:43:18 +0900

    Update protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,8 +1,6 @@
 exports.config = {
   capabilities: {
     'tunnel-identifier' : process.env.TRAVIS_JOB_NUMBER,
-    'build' : process.env.TRAVIS_BUILD_NUMBER,
-    'name' : 'build' + process.env.TRAVIS_BUILD_NUMBER,
     'browserName': 'chrome'
   },
   sauceUser: process.env.SAUCE_USERNAME,
